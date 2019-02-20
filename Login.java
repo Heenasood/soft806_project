0dec0b0aeeadb655b96a4e14daa4b35741af6ed1
Heena Sood <heena.ah9@gmail.com>
2019-02-20 18:05:57 +1300

    -wq wq n branch master

--- Login.java
+++ Login.java
@@ -1,13 +1,10 @@
-public class login
-{
-
-	public static void main(String args[])
+public static void main(String args[])
 	{
 	for (int i=1;i<=10;i++)
 		{
 		System.out.println("login Sucessfully " + i + " times");
+		System.out.println("inside loop message");
 		}
 	System.out.println("Team Farrah & Heena");
+	System.out.println("outside loop message - end code");
 	}
-	
-}
